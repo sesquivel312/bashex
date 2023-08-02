1a0abcb8b9707678a911d75e45cd0112108a69a8
nunya <nunya@nunya.nunya>
2023-08-02 14:37:33 -0700

    test webhook

--- script.sh
+++ script.sh
@@ -15,6 +15,8 @@ echo "+++ :hammer: Example tests"
 
 echo -e "\033[33mCongratulations!\033[0m You've successfully run your first build on Buildkite! 👍
 
+echo ":poop: added by me"
+
 \033[33m$(cat artifacts/thumbsup.txt)\033[0m
 
 If you have any questions or need help email support@buildkite.com, we'd be happy to help!
